3a2b0ccdfd286216767e39d9317e0a6877a55693
awesomeo184 <awesomeo184@woowahan.com>
2023-07-30 21:28:04 +0900

    initial commit

---
+++ src/main/java/com/offer/OfferApplication.java
@@ -0,0 +1,14 @@
+package com.offer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/offer/OfferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
