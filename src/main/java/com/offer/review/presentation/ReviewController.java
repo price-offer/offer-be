b6b3804674214c39e638d6459199e38ad7fb7d40
Jihun-Hwang <goharrm@gmail.com>
2023-10-09 21:04:15 +0900

    feat: swaager 추가 (spring boot 3 대응 )

--- src/main/java/com/offer/review/presentation/ReviewController.java
+++ src/main/java/com/offer/review/presentation/ReviewController.java
@@ -9,22 +9,18 @@ import com.offer.review.application.ReviewService;
 import com.offer.review.application.request.ReviewCreateRequest;
 import com.offer.review.application.response.ReviewInfoResponse;
 import com.offer.review.domain.Role;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Api(tags = "리뷰")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class ReviewController {
     private final ReviewService reviewService;
 
-    @ApiOperation(value = "특정 유저에게 리뷰 추가")
     @PostMapping("/reviews")
     public ResponseEntity<ApiResponse> createReview(@AuthenticationPrincipal LoginMember loginMember,
                                                     @RequestBody ReviewCreateRequest request) {
@@ -35,7 +31,6 @@ public class ReviewController {
         );
     }
 
-    @ApiOperation(value = "특정 유저가 받은 리뷰 전체 조회")
     @GetMapping("/reviews")
     public ResponseEntity<ApiResponse> getReviews(@AuthenticationPrincipal LoginMember loginMember,
                                                   @RequestParam(value = "memberId", required = true) Long memberId,
