5fe75d341dcb50fd371eaf6992ac26ff56a914b7
awesomeo184 <awesomeo184@woowahan.com>
2023-11-09 17:03:20 +0900

    cors mapping 추가

--- src/main/java/com/offer/config/WebConfig.java
+++ src/main/java/com/offer/config/WebConfig.java
@@ -5,13 +5,17 @@ import com.offer.authentication.presentation.AuthenticationPrincipalArgumentReso
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
 
     private final AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver;
     private final AuthenticationInterceptor authenticationInterceptor;
@@ -25,5 +29,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authenticationPrincipalArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+            .exposedHeaders(HttpHeaders.LOCATION);
     }
 }
