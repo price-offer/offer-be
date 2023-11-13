af2b0cd8db8ece8c1816b1ba031fd6d0cafea516
awesomeo184 <awesomeo184@woowahan.com>
2023-11-14 00:10:13 +0900

    SwaggerConfig 수정

--- src/main/java/com/offer/config/SwaggerConfig.java
+++ src/main/java/com/offer/config/SwaggerConfig.java
@@ -1,7 +1,12 @@
 package com.offer.config;
 
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
@@ -10,4 +15,11 @@ import org.springframework.context.annotation.Configuration;
                 version = "v1"))
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Server server = new Server();
+        server.setUrl("https://offer-be.kro.kr");
+        return new OpenAPI().servers(List.of(server));
+    }
 }
