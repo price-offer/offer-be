8ca4f7108c6ec8ede26ae9a3eba427c46938b149
awesomeo184 <awesomeo184@woowahan.com>
2023-11-19 00:08:01 +0900

    swagger 문서 정보 추가

--- src/main/java/com/offer/msg/presentation/MsgController.java
+++ src/main/java/com/offer/msg/presentation/MsgController.java
@@ -12,6 +12,7 @@ import com.offer.msg.application.request.MsgRoomCreateRequest;
 import com.offer.msg.application.response.MsgInfoResponse;
 import com.offer.msg.application.response.MsgRoomInfoResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,47 +27,57 @@ public class MsgController {
 
     @Operation(summary = "쪽지 room 생성")
     @PostMapping("/api/msgrooms")
-    public ResponseEntity<ApiResponse> createMsgRoom(@AuthenticationPrincipal LoginMember loginMember,
-                                                     @RequestBody MsgRoomCreateRequest request) {
-        CommonCreationResponse response = msgRoomService.createMsgRoom(request, loginMember.getId());
+    public ResponseEntity<ApiResponse<CommonCreationResponse>> createMsgRoom(
+        @Schema(hidden = true) @AuthenticationPrincipal LoginMember loginMember,
+        @RequestBody MsgRoomCreateRequest request) {
+
+        CommonCreationResponse response = msgRoomService.createMsgRoom(request,
+            loginMember.getId());
 
         return ResponseEntity.ok(
-                ApiResponse.of(ResponseMessage.SUCCESS, response)
+            ApiResponse.of(ResponseMessage.SUCCESS, response)
         );
     }
 
     @Operation(summary = "쪽지 room 목록 조회")
     @GetMapping("/api/msgrooms")
-    public ResponseEntity<ApiResponse> getMsgRooms(@AuthenticationPrincipal LoginMember loginMember,
-                                                   @RequestParam(required = true) int page) {
+    public ResponseEntity<ApiResponse<List<MsgRoomInfoResponse>>> getMsgRooms(
+        @Schema(hidden = true) @AuthenticationPrincipal LoginMember loginMember,
+        @RequestParam(required = true) int page) {
+
         List<MsgRoomInfoResponse> response = msgRoomService.getMsgRooms(page, loginMember.getId());
 
         return ResponseEntity.ok(
-                ApiResponse.of(ResponseMessage.SUCCESS, response)
+            ApiResponse.of(ResponseMessage.SUCCESS, response)
         );
     }
 
     @Operation(summary = "쪽지 보내기")
     @PostMapping("/api/msgrooms/{msgRoomId}/msgs")
-    public ResponseEntity<ApiResponse> sendMsg(@AuthenticationPrincipal LoginMember loginMember,
-                                               @PathVariable Long msgRoomId,
-                                               @RequestBody MsgCreateRequest request) {
-        CommonCreationResponse response = msgService.sendMsg(msgRoomId, request, loginMember.getId());
+    public ResponseEntity<ApiResponse<CommonCreationResponse>> sendMsg(
+        @Schema(hidden = true) @AuthenticationPrincipal LoginMember loginMember,
+        @PathVariable Long msgRoomId,
+        @RequestBody MsgCreateRequest request) {
+
+        CommonCreationResponse response = msgService.sendMsg(msgRoomId, request,
+            loginMember.getId());
 
         return ResponseEntity.ok(
-                ApiResponse.of(ResponseMessage.SUCCESS, response)
+            ApiResponse.of(ResponseMessage.SUCCESS, response)
         );
     }
 
     @Operation(summary = "특정 쪽지 room의 전체 쪽지 내용 조회")
     @GetMapping("/api/msgrooms/{msgRoomId}/msgs")
-    public ResponseEntity<ApiResponse> getAllMsgs(@AuthenticationPrincipal LoginMember loginMember,
-                                                  @PathVariable Long msgRoomId,
-                                                  @RequestParam(required = true) int page) {
+    public ResponseEntity<ApiResponse<List<MsgInfoResponse>>> getAllMsgs(
+        @Schema(hidden = true) @AuthenticationPrincipal LoginMember loginMember,
+        @PathVariable Long msgRoomId,
+        @RequestParam(required = true) int page) {
+
         List<MsgInfoResponse> response = msgService.getMsgs(page, msgRoomId);
 
         return ResponseEntity.ok(
-                ApiResponse.of(ResponseMessage.SUCCESS, response)
+            ApiResponse.of(ResponseMessage.SUCCESS, response)
         );
     }
 
