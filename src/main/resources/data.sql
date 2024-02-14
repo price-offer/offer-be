6e654ef346f94561e515c086156711563c34da32
awesomeo184 <awesomeo184@woowahan.com>
2024-02-14 22:53:39 +0900

    카테고리 추가 sql 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into category(code, name, image_url) values ('MEN_FASHION', '남성패션/잡화', 'https://offer-be.kro.kr/images/7e0099c4-e75d-4636-88c1-d412676bb0b9.png');
+insert into category(code, name, image_url) values ('WOMEN_FASHION', '여성패션/잡화', 'https://offer-be.kro.kr/images/ef4e0e85-ef6b-452f-9968-dc57db57a76b.png');
+insert into category(code, name, image_url) values ('GAME', '게임', 'https://offer-be.kro.kr/images/c688952b-f585-4df1-8187-d836c0280492.png');
+insert into category(code, name, image_url) values ('SPORTS', '스포츠/레저', 'https://offer-be.kro.kr/images/e287d3d3-209d-47d8-a5bd-ebde682d883e.png');
+insert into category(code, name, image_url) values ('TOY', '장난감/취미', 'https://offer-be.kro.kr/images/e218c2a1-1bd7-442f-b0f6-195a1736c62b.png');
+insert into category(code, name, image_url) values ('DIGITAL', '디지털기기', 'https://offer-be.kro.kr/images/a1126089-ec83-4cb2-8599-d507b0978749.png');
+insert into category(code, name, image_url) values ('CAR', '자동차/공구', 'https://offer-be.kro.kr/images/5920f618-de40-45da-8731-a9e4f2c9a757.png');
+insert into category(code, name, image_url) values ('APPLIANCE', '생활가전', 'https://offer-be.kro.kr/images/bccb8f2e-bf2d-4bcb-83ec-e4ea37c45cf7.png');
+insert into category(code, name, image_url) values ('FURNITURE', '가구/인테리어', 'https://offer-be.kro.kr/images/44bd7ce9-6178-41a5-a5ec-a6ec84078c97.png');
+insert into category(code, name, image_url) values ('BOOKS', '도서/티켓/음반', 'https://offer-be.kro.kr/images/0831ed40-5e99-47eb-9435-d41644f973e6.png');
+insert into category(code, name, image_url) values ('PET', '반려동물용품', 'https://offer-be.kro.kr/images/fb0d656b-5063-4125-8907-3c0b395b0975.png');
+insert into category(code, name, image_url) values ('OTHER', '기타 중고물품', 'https://offer-be.kro.kr/images/8d1db7d6-d478-4ad2-99be-c952abb201f9.png');
